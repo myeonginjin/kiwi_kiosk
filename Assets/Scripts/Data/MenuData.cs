acbc16eaf67b2a90f06b3ab8e80fc200375ba666
sete3683 <sete3683@gmail.com>
2023-05-11 01:47:59 +0900

    Removed useless scripts
    KK-7

--- Assets/Scripts/Data/MenuData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MenuData : MonoBehaviour
-{
-    public List<ItemData> menu = new List<ItemData>();
-}
